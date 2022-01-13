b099cae38b64e5a9ee350dfb207fe3fcdfdfd62f
FDiMalta <90394497+FDiMalta@users.noreply.github.com>
2022-01-13 13:14:50 +0000

    Replace the "___" by appropriate code v1, v2, v3: solution

--- covid19v3.py
+++ covid19v3.py
@@ -6,7 +6,7 @@
 # Update : 09/01/2022
 #
 # Replace the "___" by appropriate code
-#
+# End
 ###########################################
 
 import urllib.request as urllib2
@@ -16,10 +16,10 @@ from covid19v2 import *
 
 def data_for_country(data, state = "", country = ""):
     # for each country in data
-    for pays in ___:
+    for pays in data:
         # if 'Province/State' key match with state given in argument
         # and if 'Country/Region' key match with country given in argument
-        if pays[___] == ___ and pays[___] == ___:
+        if pays["Province/State"] == state and pays["Country/Region"] == country:
             return pays
 
 
